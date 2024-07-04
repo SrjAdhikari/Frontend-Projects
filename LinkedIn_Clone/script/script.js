cf44985544f7f68c5edd3271d8fecfb23637fdf7
SrjAdhikari <surajadhikari055@gmail.com>
2024-07-04 12:42:35 +0900

    Add toggle functionality

---
+++ LinkedIn_Clone/script/script.js
@@ -0,0 +1,8 @@
+// Get the element with the ID 'profileMenu'
+const profileMenu = document.getElementById('profileMenu');
+
+// Function to toggle the 'open-menu' class on the profileMenu element
+function toggleMenu() {
+    // Toggle the 'open-menu' class on the profileMenu element
+    profileMenu.classList.toggle('open-menu');
+}
