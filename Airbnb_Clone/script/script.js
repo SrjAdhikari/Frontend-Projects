688add658a14fe4aea6dc78244da8d3df459c2d4
SrjAdhikari <surajadhikari055@gmail.com>
2024-07-11 16:29:38 +0900

    Add toggle functionality for menu

---
+++ Airbnb_Clone/script/script.js
@@ -0,0 +1,7 @@
+// Get the navbar element by its ID
+const navbar = document.getElementById('navbar');
+
+// Function to toggle the 'hide-menu' class on the navbar element
+function toggleBtn() {
+    navbar.classList.toggle('hide-menu');
+}
